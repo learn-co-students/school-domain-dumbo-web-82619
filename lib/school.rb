a7a10558e1beb43713b3c51404c2fcbe2a4acac8
github username <github email address>
2019-08-29 22:55:16 -0400

    revision

--- lib/school.rb
+++ lib/school.rb
@@ -1,19 +1,14 @@
-require 'pry'
-
 class School
-  attr_reader :name
+  attr_reader :name, :roster
   
   def initialize(name)
     @name = name
     @roster = {}
   end
 
-  def roster
-    @roster
-  end
-
   def add_student(name, grade)
-    roster.include?(grade) || roster[grade] = []
+    # roster.include?(grade) || roster[grade] = []
+    roster[grade] ||= []
     roster[grade] << name
   end
 
