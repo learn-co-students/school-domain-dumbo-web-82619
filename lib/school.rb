48e1da32f1c9bab1327c570ed5a9f1b300c6423c
Aibek Ozhorov <ozhorov@gmail.com>
2019-09-02 23:08:36 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+class School
+  attr_accessor :student, :grade, :roster
+  # @roster = []
+ def initialize(school_name)
+    #@school_name = school_name
+    #empty hash 
+    @roster = {}
+  end
+  
+     def add_student(student, grade)
+    #If the grade doesn't exsist then create an empty key/pair and push grade key value into it 
+    if roster[grade]== nil 
+        roster[grade] = []
+        roster[grade] << student
+    else
+      #add students to exsisting grade 
+    roster[grade] << student
+  end
+end
+    
+     def grade(grade)
+   #retrieve all values from certain grade 
+    roster[grade]
+ end
+  def sort
+  roster.each do |key, value|
+    roster[key] = value.sort
+  end
+  end 
+  
+    
+    
+end
